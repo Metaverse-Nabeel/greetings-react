39ed8c65f0eb8e3eb074ce6048f1360dd5bceb56
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-16 20:28:56 +0500

    Greetings app front end made ready as per requirements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Greeting from "./Greeting";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-500 text-center w-full uppercase mx-8">
+        Greetings App
+      </h1>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
